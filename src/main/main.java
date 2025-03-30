b4792b9bbf0c1d1542c285d964fa678271b2c3b9
m0nica1998 <monica.cesario98@icloud.com>
2025-03-30 18:14:03 +0200

    esercizi metodi stringhe

--- src/main/main.java
+++ src/main/main.java
@@ -20,7 +20,7 @@ public class main {
 	System.out.println("Inserisci il comune di residenza");
 	scan.nextLine();
 	String comune = scan.next();
-	
+	scan.nextLine();
 	System.out.println("Inserisci la matricola");
 	int mat = scan.nextInt();
 	//usare il metodo concat( ) per stampare un messaggio come il seguente: “Gentile [nome] [cognome], nato il [data di nascita gg/mm/aaaa], residente a [comune], matricola n. [n matricola], siamo lieti di comunicarle la sua partecipazione al corso.”
@@ -28,5 +28,61 @@ public class main {
 	
 	//utilizzate almeno una volta ognuno dei seguenti metodi: 
 	//concat(), toUpperCase(), toLoweCase() e l’operatore +
+	scan.nextLine();
+	//chiedere un’email in input 
+	System.out.println("Inserisci una mail ");
+	String email = scan.next();
+	
+	//effettuare un controllo sull’email con i seguenti criteri 
+	//deve contenere una @ • deve contenere un . 
+	if(!(email.contains("@") && email.contains(".")) ) {
+		//se l’utente inserisce un’email che non rispetta i criteri va stampato un messaggio di avviso 
+		System.out.println("L'email non è valida");
+	}
+	
+	//altrimenti va stampata l’email, in lower case, togliendo eventuali spazi all’inizio   e alla fine
+	else {
+		System.out.println(email.toLowerCase().replace(" ",""));
+	}
+	//chiedere una stringa in input, verificare che contenga almeno una vocale 
+	System.out.println("Inserisci una stringa");
+	String str = scan.next();
+	if(str.contains("a")) {
+		System.out.println("la stringa contiene almeno una vocale");
+	} else if(str.contains("e")) {
+		System.out.println("la stringa contiene almeno una vocale");
+	} else if(str.contains("i")) {
+		System.out.println("la stringa contiene almeno una vocale");
+	} else if(str.contains("o")) {
+		System.out.println("la stringa contiene almeno una vocale");
+	} else if(str.contains("u")) {
+		System.out.println("la stringa contiene almeno una vocale");
+	} else {
+		System.out.println("la stringa non contiene vocali");
+	}
+	//chiedere una stringa in input, verificare che sia lunga almeno 8 caratteri 
+	
+	System.out.println("inserisci una stringa");
+	String str1 = scan.next();
+	if(str1.length() >= 8) {
+		System.out.println("lq stringa contiene almeno otto caratteri");
+	}
+	//chiedere una stringa in input, verificare che il primo carattere sia una lettera maiuscola 
+	System.out.println("inserisci una stringa");
+	String str2 = scan.next();
+    
+	if(str2.startsWith(String.valueOf(str2.charAt(0)).toUpperCase()));
+	 
+	//chiedere 3 stringhe in input per dei nomi di città, i nomi vanno stampati rimpiazzando il primo carattere con lo stesso ma in maiuscolo
+	System.out.println("inserisci una stringa per un nome di città");
+	String str3 = scan.next();
+	System.out.println("inserisci una stringa per un secondo nome di città");
+	String str4 = scan.next();
+	System.out.println("inserisci una stringa per un terzo nome di città");
+	String str5 = scan.next();
+	
+	System.out.println(String.valueOf(str3.charAt(0)).toUpperCase() + str3.substring(1).toLowerCase());
+	System.out.println(String.valueOf(str4.charAt(0)).toUpperCase() + str4.substring(1).toLowerCase());
+	System.out.println(String.valueOf(str5.charAt(0)).toUpperCase() + str5.substring(1).toLowerCase());
 	}
 }
